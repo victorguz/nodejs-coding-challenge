dee91b3f3a0e8dbda8f62729495df5872a97aeb2
Victor Guzmán Berrio <156972328+victorguz@users.noreply.github.com>
2025-09-24 23:32:28 -0500

    finish the challenge

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 const assert = require("assert");
-
+const multiply = 2;
 // Implement a function that takes an object and multiply
 // it's value by a constant and returns a promise
-// function calculateItem(item) {}
+function calculateItem(item) {
+  return new Promise((resolve) => {
+    if (!isNaN(item.value)) {
+      return resolve(Math.abs(item.value) * multiply);
+    }
+    return resolve(0);
+  });
+}
 
 // Implement a function that returns true for "settled" items
-// function isSettled(item) {}
+function isSettled(item) {
+  return item.settled;
+}
 
 async function main() {
   // Load data from a json file located at the "resources" folder
-  const data = null;
+  const data = require("../resources/data.json");
 
   // For each item in the array that is "settled"
   // Multiply its absolute value by 2
   // Sum up results
-  const calculatedValue = 0;
-  console.log("calculated value:", calculatedValue);
+  const promises = data
+    .filter((item) => isSettled(item))
+    .map((settledItem) => calculateItem(settledItem));
+  const result = await Promise.all(promises);
+  console.log(result);
 
+  const calculatedValue = result.reduce((acc, value) => acc + value, 0);
+  console.log("calculated value:", calculatedValue);
+  
   // The final answer should be 42
   assert(calculatedValue === 42, "Incorrect value");
 }
