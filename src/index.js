fd9f20313830ef4aa65370c3dbb5e18300713d35
Victor Guzmán Berrio <156972328+victorguz@users.noreply.github.com>
2025-09-24 23:32:13 -0500

    init the challenge

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const assert = require("assert");
+
+// Implement a function that takes an object and multiply
+// it's value by a constant and returns a promise
+// function calculateItem(item) {}
+
+// Implement a function that returns true for "settled" items
+// function isSettled(item) {}
+
+async function main() {
+  // Load data from a json file located at the "resources" folder
+  const data = null;
+
+  // For each item in the array that is "settled"
+  // Multiply its absolute value by 2
+  // Sum up results
+  const calculatedValue = 0;
+  console.log("calculated value:", calculatedValue);
+
+  // The final answer should be 42
+  assert(calculatedValue === 42, "Incorrect value");
+}
+
+main();
